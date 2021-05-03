a6abf5e155bd4b373a43cf5e9d74830053219cce
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2021-05-03 16:10:26 +0000

    added login page(indev)

--- ship_o_cereal/urls.py
+++ ship_o_cereal/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('catalog.urls')),
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls'))
+    path('cart/', include('cart.urls')),
+    path('login/', include('login.urls'))
 ]
