120a04acaf4a6dd428949e5579260770524c2e68
Daniels-19 <daniels19@ru.is>
2021-05-04 14:45:42 +0000

    Gerði smá virkni á description síðuna

---
+++ static/js/description.js
@@ -0,0 +1,30 @@
+let index = 0;
+function decrement() {
+    if (index > 0) index--;
+    updateAmount(index);
+    checkHowManyExist();
+}
+
+function increment() {
+    index++;
+    updateAmount(index);
+    checkHowManyExist();
+}
+
+function updateAmount(index) {
+    document.getElementById("amount").textContent = index;
+}
+
+function checkHowManyExist(){
+    if (index < 10){
+        document.getElementById("inStock").textContent = "true";
+    }
+    else{
+        document.getElementById("inStock").textContent = "false";
+    }
+}
+
+function getNameAndPrice(){
+
+}
+checkHowManyExist();
