ba29c93351961457b0a3cc2fc86ea5bf79c49486
palinalif <kollakleina@hotmail.com>
2021-05-04 14:40:02 +0000

    Added basis for card page, made URLs between pages work

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     #http://localhost:8000/cart
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    #http://localhost:8000/cart/confirm-card
+    path('confirm-card/', views.pay, name="confirm-card")
 ]
