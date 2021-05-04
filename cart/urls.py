2dc3ce58c2fd9f6fb775af5e3515fb6304a7ba80
palinalif <kollakleina@hotmail.com>
2021-05-04 14:42:26 +0000

    Changed urls.py files to have unique names so we can use them for linking

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     #http://localhost:8000/cart
-    path('', views.index, name="index"),
+    path('', views.index, name="cart-index"),
     #http://localhost:8000/cart/confirm-card
     path('confirm-card/', views.pay, name="confirm-card")
 ]
