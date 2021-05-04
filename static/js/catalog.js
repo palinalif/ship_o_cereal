c4a83af8723960f73959527d4ea97e2669e9bdf2
Garpur3 <g@rpur.net>
2021-05-04 11:37:44 +0000

    Added more cereals and post request to cart button

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,4 +1,4 @@
-cereals = {}
+let cereals = {}
 
 function decrement(cerealName) {
     if (cereals[cerealName] > 0) cereals[cerealName]--;
@@ -12,4 +12,20 @@ function increment(cerealName) {
 
 function updateAmount(cerealName) {
     document.getElementById(cerealName + " amount").textContent = cereals[cerealName];
+}
+
+function sendToCart(cerealName) {
+    $.ajax({
+        method: 'POST',
+        url: 'cart/views.py',
+        data: {'name': cerealName, 'amount': document.getElementById(cerealName + " amount").textContent},
+        success: function (data) {
+             //this gets called when server returns an OK response
+             alert("it worked!");
+             updateAmount(cerealName);
+        },
+        error: function (data) {
+             alert("it didnt work\n" + cerealName);
+        }
+    });
 }
