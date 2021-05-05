84f3878e6dd5476885b591d6daa2c084a3c44439
Garpur3 <g@rpur.net>
2021-05-05 14:49:37 +0000

    Added models to cart and products

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from products.models import Product
+from register.models import Order
 
 # Create your models here.
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order)
+    product = models.ForeignKey(Product)
+    quantity = models.IntegerField()
--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductManufacturer(models.Model):
+    name = models.CharField(max_length = 255)
+
+class ProductTag(models.Model):
+    name = models.CharField(max_length = 255)
+
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    manufacturer = models.ForeignKey(ProductManufacturer, on_delete = models.CASCADE)
+    price = models.IntegerField()
+    description = models.CharField(max_length = 255, blank = True)
+    nutriInfo = models.CharField(max_length = 255, blank = True)
+    amount = models.IntegerField()
+    tag = models.ForeignKey(ProductTag, on_delete = models.CASCADE)
+
+class ProductImage(models.Model):
+    image = models.CharField(max_length = 9999)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
