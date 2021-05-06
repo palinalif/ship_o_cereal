e1f3935fe483d35066283d1d7922befe7e7c3cc8
Daniels-19 <daniels19@ru.is>
2021-05-06 11:11:47 +0000

    Made the population script

---
+++ population_script.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO register_address ("country", "city", "houseNumber", "streetName", "postNumber") VALUES ('Iceland', 'Hafnarfjörður', 22, 'Blikaás', 221);
+INSERT INTO register_address ("country", "city", "houseNumber", "streetName", "postNumber") VALUES ('Iceland', 'Reykjavík', 16, 'Austurstræti', 101);
+INSERT INTO register_address ("country", "city", "houseNumber", "streetName", "postNumber") VALUES ('Iceland', 'Hafnarfjörður', 16, 'Breiðvangur', 220);
+INSERT INTO register_address ("country", "city", "houseNumber", "streetName", "postNumber") VALUES ('Iceland', 'Hafnarfjörður', 41, 'Miðvangur', 220);
+
+INSERT INTO register_user ("name", "email", "password", "phone", "photo", "address_id") VALUES ('Daníel Örn Sigurðsson', 'daniels19@ru.is', 'ABC.12345', 6986320, 'https://miniblogcore.azurewebsites.net/blog/some-url/', 1);
+INSERT INTO register_user ("name", "email", "password", "phone", "photo", "address_id") VALUES ('Garpur Hnévill', 'garpurH19@ru.is', 'ABC.1234', 6986321, 'https://miniblogcore.azurewebsites.net/blog/some-url/', 2);
+INSERT INTO register_user ("name", "email", "password", "phone", "photo", "address_id") VALUES ('Jón Jónsson', 'jón19@ru.is', 'ABC.123', 6986322, 'https://miniblogcore.azurewebsites.net/blog/some-url/', 3);
+INSERT INTO register_user ("name", "email", "password", "phone", "photo", "address_id") VALUES ('Gunnar Ingvarsson', 'gunnar19@ru.is', 'ABC.12', 6986323, 'https://miniblogcore.azurewebsites.net/blog/some-url/', 4);
