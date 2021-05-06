d6e93e382dcb73704c10e5f8c1b59835a7f33284
Garpur3 <g@rpur.net>
2021-05-06 14:52:30 +0000

    Catalog and products connected to db, still no sort or filtering

--- products/views.py
+++ products/views.py
@@ -3,82 +3,8 @@ from products.models import *
 
 # Create your views here.
 
-cereals = [
-    {'id': 0,
-     'name': 'Cheerios',
-     'price': 700,
-     'image': 'https://images-na.ssl-images-amazon.com/images/I/81VQIHQ0-CL._SL1500_.jpg',
-     'ingredients': 'Cheerios',
-     'nutritional': 'Carbs',
-     'label': 'Healthy'},
-
-    {'id': 1,
-     'name': 'Honey Nut Cheerios',
-     'price': 800,
-     'image': 'https://d3d71ba2asa5oz.cloudfront.net/12026801/images/honeynutcheeriosce_e780e_generalmills_alwaysfre_128__1.jpg',
-     'ingredients': 'Honey Nut Cheerios',
-     'nutritional': 'Carbs',
-     'label': 'Sugary'},
-
-    {'id': 2,
-     'name': 'Cocoa Puffs',
-     'price': 850,
-     'image': 'https://i5.walmartimages.com/asr/e78f3d25-baed-4d09-bb70-ea2ea8e3f594.93489774c0a84ad9c0170cb59ef27bc0.jpeg',
-     'ingredients': 'Cocoa Puffs',
-     'nutritional': 'Carbs',
-     'label': 'Sugary'},
-
-    {'id': 3,
-     'name': 'Lucky Charms',
-     'price': 900,
-     'image': 'https://i5.walmartimages.com/asr/66e3fc3d-32ef-41b4-9aa3-2852b3c17f19_1.f19a53b164c8a56d749fb11670983422.jpeg',
-     'ingredients': 'Lucky Charms',
-     'nutritional': 'Carbs',
-     'label': 'Sugary'},
-
-    {'id': 4,
-     'name': 'Havre Fras',
-     'price': 750,
-     'image': 'https://cdn.shopify.com/s/files/1/1518/6272/products/To-1.01_Flingorhavrefrasoriginal_1024x1024.jpg?v=1520345667',
-     'ingredients': 'Havre Fras',
-     'nutritional': 'Carbs',
-     'label': 'Healthy'},
-
-    {'id': 5,
-     'name': 'Cinnamon Toast Crunch',
-     'price': 800,
-     'image': 'https://www.usa-drinks.de/media/image/product/12475/lg/cinnamon-toast-crunch-1-x-340g.jpg',
-     'ingredients': 'Cinnamon Toast Crunch',
-     'nutritional': 'Carbs',
-     'label': 'Sugary'},
-
-    {'id': 6,
-     'name': 'Trix',
-     'price': 850,
-     'image': 'https://i5.walmartimages.com/asr/20011f44-8b7e-4750-a37a-236fbba4e54c_1.9b22a7ddf3a4198b5e8f2a7cc7836557.jpeg',
-     'ingredients': 'Trix',
-     'nutritional': 'Carbs',
-     'label': 'Sugary'},
-
-    {'id': 7,
-     'name': 'Fruit Loops',
-     'price': 800,
-     'image': 'https://www.frajosfood.com/img/p/3/7/0/0/8/37008-thickbox_default.jpg',
-     'ingredients': 'Fruit Loops',
-     'nutritional': 'Carbs',
-     'label': 'Sugary'},
-
-    {'id': 8,
-     'name': 'Rice Krispies',
-     'price': 650,
-     'image': 'https://images-na.ssl-images-amazon.com/images/I/91EbJNh%2BkuL._SL1500_.jpg',
-     'ingredients': 'Rice Krispies',
-     'nutritional': 'Carbs',
-     'label': 'Healthy'},
-]
-
 def getCereals():
-    return cereals
+    return Product.objects.all()
 
 def getSortedBy(strin):
     return sorted(cereals, key = lambda i: i[strin])
@@ -93,4 +19,4 @@ def getFilterBy(strin):
 
 def product(request, id):
     # TODO: make id check and if invalid goto 404 page
-    return render(request, 'products/index.html', context=({'cereal': cereals[id]}))
+    return render(request, 'products/index.html', context=({'cereal': getCereals().get(id=id)}))
