10635902c1511c8738a59dbc9b133dbf9793c9da
Garpur3 <g@rpur.net>
2021-05-07 15:19:22 +0000

    removed login app and moved it to user

--- user/views.py
+++ user/views.py
@@ -13,3 +13,24 @@ def register(request):
     return render(request, 'user/register.html', {
         'form': AccountRegisterForm()
     })
+
+def index(request):
+    return render(request, 'login/login.html')
+
+def loginUser(request):
+    #https://www.smashingmagazine.com/2020/02/django-highlights-user-models-authentication/
+    if request.user.is_authenticated:
+        return render(request, 'catalog/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            form = AuthenticationForm(request.POST)
+            return render(request, 'login/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login/login.html', {'form': form})
