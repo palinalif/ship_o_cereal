d7818af3416004f08f6891d7855e3ceb5cc7e8ef
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2021-05-07 15:45:08 +0000

    Adjusting register

--- user/forms/accountForm.py
+++ user/forms/accountForm.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm, widgets
+from django import forms
 from user.models import User
 
 class AccountRegisterForm(ModelForm):
@@ -7,13 +8,13 @@ class AccountRegisterForm(ModelForm):
         exclude = ['id']
         widgets = {
             'email': widgets.TextInput(attrs = {'class': 'form-control'}),
-            'password': widgets.TextInput(attrs = {'class': 'form-control'}),
+            'password': widgets.PasswordInput(attrs = {'class': 'form-control'}),
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'photo': widgets.TextInput(attrs={'class': 'form-control'}),
+            'photo': widgets.FileInput(attrs={'class': 'form-control'}),
             'streetName': widgets.TextInput(attrs = {'class': 'form-control'}),
             'houseNumber': widgets.TextInput(attrs = {'class': 'form-control'}),
             'postNumber': widgets.TextInput(attrs = {'class': 'form-control'}),
             'city': widgets.TextInput(attrs = {'class': 'form-control'}),
-            'country': widgets.TextInput(attrs = {'class': 'form-control'}),
+            'country': widgets.Select(attrs = {'class': 'form-control'}),
             'phone': widgets.TextInput(attrs={'class': 'form-control'}),
         }
