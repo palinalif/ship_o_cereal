0ce5c5622ef381427c5e5d89b6fc211a53a357fd
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2021-05-12 09:56:56 +0000

    Fixed login

--- user/forms/accountForm.py
+++ user/forms/accountForm.py
@@ -7,7 +7,7 @@ class AccountRegisterForm(ModelForm):
         model = User
         exclude = ['id']
         widgets = {
-            'email': widgets.TextInput(attrs = {'class': 'form-control'}),
+            'email': widgets.EmailInput(attrs = {'class': 'form-control'}),
             'password': widgets.PasswordInput(attrs = {'class': 'form-control'}),
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'photo': widgets.FileInput(attrs={'class': 'form-control'}),
