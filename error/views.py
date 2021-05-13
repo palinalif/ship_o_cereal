0081ef1149a8b3a42483b9876c88b2edcc2e853b
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2021-05-13 18:02:16 +0000

    Added endpoints

--- error/views.py
+++ error/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def error_400_view(request, exception):
+    return render(request, '400.html')
+
+def error_403_view(request, exception):
+    return render(request, '403.html')
+
 def error_404_view(request, exception):
     return render(request, '404.html')
+
+def error_500_view(request):
+    return render(request, '500.html')
